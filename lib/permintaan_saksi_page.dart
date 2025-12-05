4b04d8569c465540bb74e0899f5aa25b4f27c132
gilban-faresy <muhammadikmalgilbanfaresy@gmail.com>
2025-12-05 10:58:27 +0700

    update version

--- lib/permintaan_saksi_page.dart
+++ lib/permintaan_saksi_page.dart
@@ -37,13 +37,88 @@ class _PermintaanSaksiPageState extends State<PermintaanSaksiPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+     drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              leading: Icon(Icons.attach_money_sharp),
+              title: Text("Dashboard"),
+              onTap: () {
+                Navigator.pushNamed(context, "/dashboard");
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Profile"),
+              onTap: () {
+                Navigator.pushNamed(context, "/profile");
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.explore_outlined),
+              title: Text("Explore"),
+              onTap: () {
+                Navigator.pushNamed(context, "/explore");
+              },
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.note),
+              title: Text("Jurnal Pembisaan"),
+              onTap: () {
+                Navigator.pushNamed(context, "/jurnal-pembiasaan");
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.person_2),
+              title: Text("Permintaan Saksi"),
+              onTap: () {
+                Navigator.pushNamed(context, "/permintaan-saksi");
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.camera_alt_sharp),
+              title: Text("Progress"),
+              onTap: () {
+                Navigator.pushNamed(context, "/progress");
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings_sharp),
+              title: Text("Catatan Sikap"),
+              onTap: () {
+                Navigator.pushNamed(context, "/catatan-sikap");
+              },
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.book_online),
+              title: Text("Panduan Penggunaan"),
+              onTap: () {
+                Navigator.pushNamed(context, "/panduan-penggunaan");
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings_sharp),
+              title: Text("Setting"),
+              onTap: () {
+                Navigator.pushNamed(context, "/settings");
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text("Logout"),
+              onTap: () {
+                Navigator.pushNamed(context, "/");
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white,
-        leading: const Padding(
-          padding: EdgeInsets.only(left: 10),
-          child: Icon(Icons.home_outlined, color: Colors.black),
-        ),
+        leading: Icon(Icons.home, color: Colors.black, size: 22),
         actions: [
           Padding(
             padding: const EdgeInsets.only(right: 10),
@@ -54,7 +129,7 @@ class _PermintaanSaksiPageState extends State<PermintaanSaksiPage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
-                      "Dennis Dwi Musti",
+                      "Ikmal Gilban Faresy",
                       style: TextStyle(
                         color: Colors.black,
                         fontWeight: FontWeight.bold,
@@ -67,8 +142,22 @@ class _PermintaanSaksiPageState extends State<PermintaanSaksiPage> {
                     ),
                   ],
                 ),
-                const SizedBox(width: 8),
+                SizedBox(width: 8),
                 Icon(Icons.account_circle, color: Colors.black, size: 40),
+                Builder(
+                  builder: (context) => IconButton(
+                    icon: Icon(
+                      Icons.arrow_drop_down,
+                      color: Colors.black,
+                      size: 20,
+                    ),
+                    padding: EdgeInsets.zero,
+                    constraints: const BoxConstraints(),
+                    onPressed: () {
+                      Scaffold.of(context).openDrawer();
+                    },
+                  ),
+                ),
               ],
             ),
           ),
@@ -79,6 +168,7 @@ class _PermintaanSaksiPageState extends State<PermintaanSaksiPage> {
           child: Container(color: Colors.grey.shade300, height: 1),
         ),
       ),
+      
 
       body: SingleChildScrollView(
         child: Padding(
--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -9,42 +9,135 @@ class ProfilePage extends StatelessWidget {
       length: 3,
       child: Scaffold(
         backgroundColor: Colors.white,
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              ListTile(
+                leading: Icon(Icons.attach_money_sharp),
+                title: Text("Dashboard"),
+                onTap: () {
+                  Navigator.pushNamed(context, "/dashboard");
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Profile"),
+                onTap: () {
+                  Navigator.pushNamed(context, "/profile");
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.explore_outlined),
+                title: Text("Explore"),
+                onTap: () {
+                  Navigator.pushNamed(context, "/explore");
+                },
+              ),
+              Divider(),
+              ListTile(
+                leading: Icon(Icons.note),
+                title: Text("Jurnal Pembisaan"),
+                onTap: () {
+                  Navigator.pushNamed(context, "/jurnal-pembiasaan");
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.person_2),
+                title: Text("Permintaan Saksi"),
+                onTap: () {
+                  Navigator.pushNamed(context, "/permintaan-saksi");
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.camera_alt_sharp),
+                title: Text("Progress"),
+                onTap: () {
+                  Navigator.pushNamed(context, "/progress");
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.settings_sharp),
+                title: Text("Catatan Sikap"),
+                onTap: () {
+                  Navigator.pushNamed(context, "/catatan-sikap");
+                },
+              ),
+              Divider(),
+              ListTile(
+                leading: Icon(Icons.book_online),
+                title: Text("Panduan Penggunaan"),
+                onTap: () {
+                  Navigator.pushNamed(context, "/panduan-penggunaan");
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.settings_sharp),
+                title: Text("Setting"),
+                onTap: () {
+                  Navigator.pushNamed(context, "/settings");
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.logout),
+                title: Text("Logout"),
+                onTap: () {
+                  Navigator.pushNamed(context, "/");
+                },
+              ),
+            ],
+          ),
+        ),
         appBar: AppBar(
+          elevation: 0,
           backgroundColor: Colors.white,
           leading: Icon(Icons.home, color: Colors.black, size: 22),
           actions: [
-            Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Ibrahim Satria",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w600,
+            Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: Row(
+                children: [
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Ikmal Gilban Faresy",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                        ),
                       ),
+                      Text(
+                        "PPLG XII-3",
+                        style: TextStyle(color: Colors.black87, fontSize: 12),
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 8),
+                  Icon(Icons.account_circle, color: Colors.black, size: 40),
+                  Builder(
+                    builder: (context) => IconButton(
+                      icon: Icon(
+                        Icons.arrow_drop_down,
+                        color: Colors.black,
+                        size: 20,
+                      ),
+                      padding: EdgeInsets.zero,
+                      constraints: const BoxConstraints(),
+                      onPressed: () {
+                        Scaffold.of(context).openDrawer();
+                      },
                     ),
-                    Text(
-                      "PPLG XII-3",
-                      style: TextStyle(color: Colors.grey, fontSize: 12),
-                    ),
-                  ],
-                ),
-                SizedBox(width: 10),
-                CircleAvatar(
-                  radius: 18,
-                  backgroundImage: AssetImage("assets/images/profile.JPG"),
-                ),
-                SizedBox(width: 12),
-              ],
+                  ),
+                ],
+              ),
             ),
+            const SizedBox(width: 5),
           ],
           bottom: PreferredSize(
-            preferredSize: Size.fromHeight(1),
-            child: Container(color: Colors.grey, height: 1),
+            preferredSize: const Size.fromHeight(1),
+            child: Container(color: Colors.grey.shade300, height: 1),
           ),
         ),
 
